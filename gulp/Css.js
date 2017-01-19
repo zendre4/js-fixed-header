3291a56fda673ef064e8ce5c65995a6146c3d100
julien <julien.stalder@vysual.ch>
2017-01-19 09:40:58 +0100

    remove not necessary file

--- gulp/Css.js
+++ gulp/Css.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 
 gulp.task('generateDistCss', function() {
@@ -10,10 +9,8 @@ gulp.task('generateDistCss', function() {
     return  gulp.src(
         "./src/css/js-fixed-header.scss"
     )
-        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle:"compressed"}))
         .pipe(rename({suffix: ".min"}))
-        .pipe(sourcemaps.write("./", {includeContent: false, sourceRoot: "../scss"}))
         .pipe(gulp.dest("./dist"));
 
 
